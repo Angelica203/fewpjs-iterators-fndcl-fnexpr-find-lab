11d986399c0da296f625c66c3f397f040195e0f7
github username <github email address>
2021-06-22 20:05:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+superbowlWin = (record) =>{
+    let result = record.find( record => record.result === "W")
+    return !!result ? result.year : undefined
+}
 
+// record.find( function(s) {return s ===win})
+
+// // const titleCased = (input) => {
+// //     return tutorials.map(tutorial => {
+// //       return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+// //     })
+  
